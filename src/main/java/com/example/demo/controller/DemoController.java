1a7463d688d3277f432fc8be430f3fe55e9a3da8
20211023 <113007205+20211023@users.noreply.github.com>
2024-10-16 01:46:23 +0900

    4주차_개인과제까지완료

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -53,7 +53,13 @@ public class DemoController {
     public String getAllTestDBs(Model model) {
     TestDB test = testService.findByName("홍길동");
     model.addAttribute("data4", test);
+    TestDB test2 = testService.findByName("아저씨");
+    model.addAttribute("data5", test2);
+    TestDB test3 = testService.findByName("꾸러기");
+    model.addAttribute("data6", test3);
     System.out.println("데이터 출력 디버그 : " + test);
+    System.out.println("데이터 출력 디버그 : " + test2);
+    System.out.println("데이터 출력 디버그 : " + test3);
     return "testdb";
     }
 
