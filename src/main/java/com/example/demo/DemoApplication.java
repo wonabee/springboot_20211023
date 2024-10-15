4ad83c9538bb0395e1df55a91cbfec10e93ae388
20211023 <113007205+20211023@users.noreply.github.com>
2024-10-15 12:23:14 +0900

    4주차_테스트페이지(32페이지까지)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
-package com.example.demo; // 현재 폴더 위치
+package com.example.demo;
+
 import org.springframework.boot.SpringApplication; // 스프링 핵심 클래스
 import org.springframework.boot.autoconfigure.SpringBootApplication; // 자동 설정 기능 활성화
-@SpringBootApplication // 애노테이션(스프링 부트 APP 명시, 하위 다양한 설정을 자동 등록)
-public class DemoApplication { // 클래스 이름
-public static void main(String[] args) { // 메인 메서드(프로그램 시작점)
-SpringApplication.run(DemoApplication.class, args); // run 메서드로 실행
-}
+
+@SpringBootApplication // 에노테이션(스프링부트 app 명시, 하위 다양한 설정 자동 등록)
+public class DemoApplication { // 클래스 이름4
+
+	//메인 메서드(프로그램의 시작점)
+	public static void main(String[] args) { 
+		SpringApplication.run(DemoApplication.class, args); // run 메서드로 실행
+	}
+
 }
---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,64 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.example.demo.model.domain.TestDB;
+import com.example.demo.model.service.TestService;
+
+@Controller
+public class DemoController {
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "반갑습니다.");
+        return "hello";
+    }
+
+    // 2주차 연습문제
+    @GetMapping("/hello2") // 전송 방식 GET
+    
+    public String hello2(Model model) {
+        model.addAttribute("data1", " 홍길동님.");
+        model.addAttribute("data2", " 반갑습니다.");
+        model.addAttribute("data3", " 오늘.");
+        model.addAttribute("data4", " 날씨는.");
+        model.addAttribute("data5", " 매우 좋습니다.");
+        return "hello2"; 
+    }
+
+    // 3주차 about_detailed
+    @GetMapping("/about_detailed")
+    public String about() {
+        return "about_detailed";
+    }
+
+    //4주차
+    @GetMapping("/thymeleaf_test1")
+    public String thymeleaf_test1(Model model) {
+    model.addAttribute("data1", "<h2> 방갑습니다 </h2>");
+    model.addAttribute("data2", "태그의 속성 값");
+    model.addAttribute("link", 01);
+    model.addAttribute("name", "홍길동");
+    model.addAttribute("para1", "001");
+    model.addAttribute("para2", 002);
+    return "thymeleaf_test1";
+    }
+
+    @Autowired
+    TestService testService;
+    
+    @GetMapping("/testdb")
+    // 클래스 필드에 추가
+    public String getAllTestDBs(Model model) {
+    TestDB test = testService.findByName("홍길동");
+    model.addAttribute("data4", test);
+    System.out.println("데이터 출력 디버그 : " + test);
+    return "testdb";
+    }
+
+    
+    
+    
+}
